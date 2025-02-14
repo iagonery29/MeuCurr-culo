b8c6cd19715cb39ee4e16c975f7ef3a80b88478a
iagonery29 <iagonery@gmail.com>
2025-02-14 12:22:23 -0300

    interacao.js

---
+++ interacao.js
@@ -0,0 +1,8 @@
+function mudarTema() {
+    document.getElementById("head").style.backgroundColor = '#006494';
+    document.getElementById("head").style.color = '#E8F1F2';
+    document.getElementById("navegador").style.backgroundColor = '#13293D';
+    document.getElementById("principal").style.backgroundColor = '#E8F1F2';
+    document.getElementById("foot").style.backgroundColor = '#006494';
+    document.getElementById("foot").style.color = '#E8F1F2';
+}
